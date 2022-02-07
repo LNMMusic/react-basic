74a86a9c07284fc064416bc4c803e91c02464884
Lucas Masiero <lucas.masi.ero@hotmail.com>
2022-02-07 14:33:08 -0300

    new start for react base

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-// Global
+// GLOBAL
 import React from 'react'; import ReactDOM from 'react-dom';
+// CSS                                                        ->  HTML head
 import 'normalize.css'; import './index.css';
 
 
-// APP [Component]
+// APP [Component]                                            ->  HTML body root
 import App from './App';
 
 // RENDER [root]
