b51cf07817260556be71e67eed1e1ddea4fc2828
Lucas Masiero <lucas.masi.ero@hotmail.com>
2022-02-07 14:22:44 -0300

    base react app to start

---
+++ src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// components
+// ...
+
+
+
+
+const HomePage = () => {
+    
+  
+  
+  
+    return (<>
+        <h3>
+            Hello World!
+        </h3>
+    </>)
+};
+
+export default HomePage;
